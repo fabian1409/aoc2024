b3394f2c1afc588b770af6917c509d5cdb7aabba
Fabian Gruber <fabian.gruber5@gmail.com>
2024-12-05 21:11:17 +0100

    day05

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,24 +1,35 @@
-use std::collections::HashSet;
-
 use aoc_traits::AdventOfCodeDay;
+use intmap::{Entry, IntMap};
 use itertools::Itertools;
+
+fn valid(line: &[u32], rules: &IntMap<Vec<u32>>) -> bool {
+    line.iter().tuple_windows::<(_, _)>().all(|pair| {
+        rules
+            .get(*pair.0 as u64)
+            .is_some_and(|follow| follow.contains(pair.1))
+    })
+}
 
 #[derive(Default)]
 pub struct Solver;
 impl AdventOfCodeDay for Solver {
-    type ParsedInput<'a> = (HashSet<(u32, u32)>, Vec<Vec<u32>>);
+    type ParsedInput<'a> = (IntMap<Vec<u32>>, Vec<Vec<u32>>);
     type Part1Output = u32;
     type Part2Output = u32;
 
     fn parse_input(input: &str) -> Self::ParsedInput<'_> {
-        let (rules, updates) = input.split_once("\n\n").unwrap();
-        let rules = rules
-            .lines()
-            .map(|line| {
-                let (a, b) = line.split_once('|').unwrap();
-                (a.parse().unwrap(), b.parse().unwrap())
-            })
-            .collect();
+        let (rules_, updates) = input.split_once("\n\n").unwrap();
+        let mut rules = IntMap::new();
+        for rule in rules_.lines() {
+            let (a, b) = rule.split_once('|').unwrap();
+            let a = a.parse().unwrap();
+            let b = b.parse().unwrap();
+            let follow = match rules.entry(a) {
+                Entry::Occupied(entry) => entry.into_mut(),
+                Entry::Vacant(entry) => entry.insert(vec![]),
+            };
+            follow.push(b);
+        }
         let updates = updates
             .lines()
             .map(|line| line.split(',').map(|p| p.parse().unwrap()).collect())
@@ -33,11 +44,7 @@ impl AdventOfCodeDay for Solver {
         updates
             .iter()
             .map(|line| {
-                if line
-                    .iter()
-                    .tuple_windows::<(_, _)>()
-                    .all(|pair| rules.contains(&(*pair.0, *pair.1)))
-                {
+                if valid(line, rules) {
                     line[line.len() / 2]
                 } else {
                     0
@@ -47,7 +54,33 @@ impl AdventOfCodeDay for Solver {
     }
 
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        todo!()
+        let rules = &input.0;
+        let updates = &input.1;
+
+        updates
+            .iter()
+            .map(|line| {
+                if !valid(line, rules) {
+                    let len = line.len();
+                    let mut line = line.to_vec();
+                    while !valid(&line, rules) {
+                        for i in 0..len - 1 {
+                            if rules
+                                .get(line[i] as u64)
+                                .is_some_and(|follow| !follow.contains(&line[i + 1]))
+                                || rules.get(line[i] as u64).is_none()
+                            {
+                                line.swap(i, i + 1);
+                                break;
+                            }
+                        }
+                    }
+                    line[line.len() / 2]
+                } else {
+                    0
+                }
+            })
+            .sum()
     }
 }
 
@@ -95,6 +128,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let parsed = Solver::parse_input(INPUT);
-        assert_eq!(Solver::solve_part2(&parsed), 31);
+        assert_eq!(Solver::solve_part2(&parsed), 123);
     }
 }
